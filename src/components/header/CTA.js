e83698664dcb0f1190593d8b4096ef045ef00975
Konstantin Nikolaev <konstantinnikolaev@Konstantins-MacBook-Pro.local>
2022-09-06 20:16:24 -0700

    CV adjustment

--- src/components/header/CTA.js
+++ src/components/header/CTA.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CV from "./cv.pdf";
 
 export default function CTA() {
   return (
     <div className="header-btn-container">
-      <a className="btn" href="/">
+      <a className="btn" href={CV} target="blank">
         Download CV
       </a>
       <Link className="btn btn-secondary" to="/contact">
